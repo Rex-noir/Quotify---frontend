3c10a158ca39a9184d6f1d1a3736f18e6b30e61b
Lala <ace.rexnoir@gmail.com>
2024-07-06 12:58:57 +0530

    feat : initial commit

---
+++ src/views/Home/PostsView.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { Post } from "@/types/Post/post.types";
+import DataView from "primevue/dataview";
+import { ref } from "vue";
+import CardPost from "@/components/Posts/CardPost.vue";
+
+const posts = ref<Post[]>([
+  {
+    username: "Phengphon",
+    title: "This is the title",
+    quote: "This is the quotehhhfsf",
+  },
+  {
+    username: "hi",
+  },
+]);
+</script>
+
+<template>
+  <main>
+    <DataView :value="posts">
+      <template #list="slotProp">
+        <CardPost
+          v-for="(post, index) in slotProp.items"
+          :post="post as Post"
+        ></CardPost>
+      </template>
+    </DataView>
+  </main>
+</template>
