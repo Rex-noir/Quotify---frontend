8c22d5c6530b3edf4b06703d3c7be039980671ed
Lala <ace.rexnoir@gmail.com>
2024-07-07 20:00:46 +0530

    feat : add search bar in large screens

--- src/components/Headers/HeaderLarge.vue
+++ src/components/Headers/HeaderLarge.vue
@@ -1,13 +1,63 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import HeaderTabs from "./HeaderTabs.vue";
+import InputText from "primevue/inputtext";
+import Button from "primevue/button";
+
+const searchFocus = ref<boolean>(false);
+const showButton = ref<boolean>(false);
 </script>
 <template>
-  <div class="grid-cols-3 border-b p-1">
-    <div class="col-start-1 text-center">
-      <span class="font-display text-2xl font-bold">Quotify</span>
+  <div class="grid-cols-4 items-center gap-10 border-b p-1">
+    <div class="relative">
+      <div class="col-start-1 flex items-center gap-3 text-center">
+        <Transition name="fade" @after-leave="showButton = true">
+          <span v-if="!searchFocus" class="font-display text-2xl font-bold"
+            >Quotify</span
+          >
+        </Transition>
+        <Button
+          v-if="showButton"
+          outlined
+          class="border-none bg-surface-200"
+          size="small"
+          icon="pi pi-arrow-left "
+        ></Button>
+        <InputText
+          @focus="searchFocus = true"
+          @focusout="
+            searchFocus = false;
+            showButton = false;
+          "
+          size="small"
+          class="h-fit w-full"
+          placeholder="Search"
+        />
+      </div>
+      <div
+        v-if="searchFocus"
+        class="absolute mt-3 w-full rounded-sm bg-surface-200 p-2"
+      >
+        Search View
+      </div>
     </div>
     <div class="col-span-3 col-start-2">
       <HeaderTabs />
     </div>
   </div>
 </template>
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.5s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  transform: translateX(-100%);
+  transition: all 0.2s ease;
+}
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+}
+</style>
