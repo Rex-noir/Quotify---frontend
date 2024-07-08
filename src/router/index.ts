6076455d2acd07e6a0ae1b9a542c246be0729e42
Lala <ace.rexnoir@gmail.com>
2024-07-08 18:20:40 +0530

    feat : add sign up route

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { LayoutNames } from "@/types/Layouts/layouts.types";
 import PostViews from "@/views/Home/PostsView.vue";
+import SignUpView from "@/views/Auth/SignUpView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +11,12 @@ const router = createRouter({
       name: "home",
       component: PostViews,
       meta: { layout: LayoutNames.HOME },
+    },
+    {
+      path: "/signup",
+      name: "signup",
+      component: SignUpView,
+      meta: { layout: LayoutNames.AUTH },
     },
   ],
 });
