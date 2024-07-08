d7cf7727eddb87f6c95d214c030070edcbdb62ed
Lala <ace.rexnoir@gmail.com>
2024-07-08 21:57:39 +0530

    refactor : move Logo Title right corner

--- src/components/Headers/HeaderLarge.vue
+++ src/components/Headers/HeaderLarge.vue
@@ -3,37 +3,28 @@ import { ref } from "vue";
 import HeaderTabs from "./HeaderTabs.vue";
 import InputText from "primevue/inputtext";
 import Button from "primevue/button";
+import InputIcon from "primevue/inputicon";
+import IconField from "primevue/iconfield";
 
 const searchFocus = ref<boolean>(false);
-const showButton = ref<boolean>(false);
 </script>
 <template>
-  <div class="grid-cols-4 items-center gap-10 border-b p-1">
-    <div class="relative">
-      <div class="col-start-1 flex items-center gap-3 text-center">
-        <Transition name="fade" @after-leave="showButton = true">
-          <span v-if="!searchFocus" class="font-display text-2xl font-bold"
-            >Quotify</span
-          >
-        </Transition>
-        <Button
-          v-if="showButton"
-          outlined
-          class="border-none bg-surface-200"
-          size="small"
-          icon="pi pi-arrow-left "
-        ></Button>
+  <div class="grid-cols-[250px,1fr,1fr,1fr] items-center gap-10 border-b p-1">
+    <div class="prose relative">
+      <IconField>
+        <InputIcon
+          v-show="searchFocus"
+          class="pi pi-arrow-left hover:cursor-pointer"
+        />
         <InputText
           @focus="searchFocus = true"
-          @focusout="
-            searchFocus = false;
-            showButton = false;
-          "
+          @focusout="searchFocus = false"
           size="small"
-          class="h-fit w-full"
           placeholder="Search"
+          class="h-fit w-full pl-10"
         />
-      </div>
+        <InputIcon class="pi pi-search hover:cursor-pointer" />
+      </IconField>
       <div
         v-if="searchFocus"
         class="absolute mt-3 w-full rounded-sm bg-surface-200 p-2"
@@ -41,23 +32,11 @@ const showButton = ref<boolean>(false);
         Search View
       </div>
     </div>
-    <div class="col-span-3 col-start-2">
+    <div class="col-span-2 col-start-2">
       <HeaderTabs />
     </div>
+    <span class="col-start-4 text-center font-display text-2xl font-bold"
+      >Quotify</span
+    >
   </div>
 </template>
-<style scoped>
-.fade-enter-active,
-.fade-leave-active {
-  transition: all 0.5s ease;
-}
-.fade-enter-from,
-.fade-leave-to {
-  transform: translateX(-100%);
-  transition: all 0.2s ease;
-}
-.fade-enter-to,
-.fade-leave-from {
-  opacity: 1;
-}
-</style>
