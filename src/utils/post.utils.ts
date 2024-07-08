ac46bd2aa4fcbbdbc9cc50db3a963893b6531816
Lala <ace.rexnoir@gmail.com>
2024-07-08 18:19:43 +0530

    refactor : change fn name

--- src/utils/post.utils.ts
+++ src/utils/post.utils.ts
@@ -1,10 +1,9 @@
 import api from "@/config/api.config";
 import type { Post } from "@/types/Post/post.types";
 import type { PaginatedResponse } from "@/types/Response/apiresponses.types";
-import type { AxiosResponse } from "axios";
 
 export default class PostUtils {
-  static async fetchPostLists(url?: string): Promise<PaginatedResponse<Post>> {
+  static async fetchPosts(url?: string): Promise<PaginatedResponse<Post>> {
     try {
       const response = await api.get(url || "/posts");
       return response.data;
