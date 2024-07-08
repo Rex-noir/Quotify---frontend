394142bf01f5dd974293c04e55cabc467415fb51
Lala <ace.rexnoir@gmail.com>
2024-07-08 18:18:03 +0530

    feat : add 'X-Requested-With' header

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,9 +1,11 @@
-import axios from 'axios'
+import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
-  withXSRFToken: true
-})
-
-export default api
+  headers: {
+    "X-Requested-With": "XMLHttpRequest",
+  },
+  withXSRFToken: true,
+});
+export default api;
