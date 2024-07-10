2bb6401ed7e0a9b36ba8bb46456cc363f5873e05
Lala <ace.rexnoir@gmail.com>
2024-07-10 15:01:52 +0530

    refactor : add padding

--- src/views/Home/CommunitiesView.vue
+++ src/views/Home/CommunitiesView.vue
@@ -4,5 +4,9 @@ const items = ref(Array.from({ length: 30 }).map((_, i) => `Community #${i}`));
 </script>
 
 <template>
-  <div class="prose h-full w-full dark:prose-invert prose-h2:mb-0">hi</div>
+  <div
+    class="prose h-full p-2 w-full dark:prose-invert prose-h2:mb-0 dark:bg-surface-0"
+  >
+    hi
+  </div>
 </template>
