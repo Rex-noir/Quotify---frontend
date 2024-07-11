d5fca89ca5f37da6982ab28d9034f166a52f0ec5
Lala <ace.rexnoir@gmail.com>
2024-07-11 13:31:51 +0530

    fix  : bg not matching with the theme

--- src/views/Home/CommunitiesView.vue
+++ src/views/Home/CommunitiesView.vue
@@ -5,7 +5,7 @@ const items = ref(Array.from({ length: 30 }).map((_, i) => `Community #${i}`));
 
 <template>
   <div
-    class="prose h-full p-2 w-full dark:prose-invert prose-h2:mb-0 dark:bg-surface-0"
+    class="prose h-full w-full p-2 dark:prose-invert prose-h2:mb-0 dark:bg-surface-0"
   >
     hi
   </div>
