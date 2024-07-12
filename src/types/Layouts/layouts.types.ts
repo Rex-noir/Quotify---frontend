25c580f7ff708a257ec8d506f2239c3a2652357b
Lala <ace.rexnoir@gmail.com>
2024-07-12 22:05:29 +0530

    feat : add PostLayout

--- src/types/Layouts/layouts.types.ts
+++ src/types/Layouts/layouts.types.ts
@@ -1,15 +1,18 @@
 import HomeLayout from "@/Layouts/HomeLayout.vue";
 import AuthLayout from "@/Layouts/AuthLayout.vue";
 import SplashLayout from "@/Layouts/SplashLayout.vue";
+import PostLayout from "@/Layouts/PostLayout.vue";
 import type { DefineComponent } from "vue";
 
 export enum LayoutNames {
   HOME = "HOME",
   AUTH = "AUTH",
   SPLASH = "SPLASH",
+  POST = "POST",
 }
 export const layouts: { [key in LayoutNames]: DefineComponent } = {
   [LayoutNames.HOME]: HomeLayout as DefineComponent,
   [LayoutNames.AUTH]: AuthLayout as DefineComponent,
   [LayoutNames.SPLASH]: SplashLayout as DefineComponent,
+  [LayoutNames.POST]: PostLayout as DefineComponent,
 };
