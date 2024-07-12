7f01a5845b109e9292026926f380d3aa75f27229
Lala <ace.rexnoir@gmail.com>
2024-07-12 22:01:13 +0530

    revert : to none-round card feat : view quotes on new Layout when clicked

--- src/components/Posts/CardPost.vue
+++ src/components/Posts/CardPost.vue
@@ -1,72 +1,98 @@
 <script setup lang="ts">
-import { PostStyles, type Post } from "@/types/Post/post.types";
+import { type Post } from "@/types/Post/post.types";
 import Panel from "primevue/panel";
 import Avatar from "primevue/avatar";
 import PostActions from "./PostActionsBar.vue";
 import { ref } from "vue";
-import usePostStore from "@/stores/posts.store";
 import dayjs from "dayjs";
+import usePostStore from "@/stores/posts.store";
+import { useRouter } from "vue-router";
 
-defineProps<{ post: Post }>();
+const router = useRouter();
+
+const props = defineProps<{ post: Post }>();
 const postStore = usePostStore();
+
 let collapsed = ref(false);
+
+const viewPost = () => {
+  postStore.setCurrentPost(props.post);
+  router.push({
+    name: "viewQuote",
+    params: { id: props.post.id },
+  });
+};
+
+const headerClickFilter = (e: Event) => {
+  const headerDiv = document.querySelector(`div#header-${props.post.id}`);
+  if (e.target === headerDiv) {
+    viewPost();
+  }
+};
 </script>
 <template>
   <div>
-    <div class="w-full p-2">
+    <div class="w-full">
       <Panel
         toggleable
-        :class="[
-          { 'rounded-xl': postStore.post_style === PostStyles.CARD_ROUND },
-        ]"
-        class="border-none shadow-md dark:bg-[#272626]"
+        class="mb-1 cursor-pointer divide-purple-400 rounded-none border-none bg-surface-100 shadow-md transition-all dark:bg-[#2d2a2a]"
       >
         <template #toggleicon>
-          <span
-            v-if="collapsed"
-            @click="collapsed = !collapsed"
-            class="pi pi-chevron-down"
-          ></span>
-          <span
-            v-else
-            @click="collapsed = !collapsed"
-            class="pi pi-chevron-up"
-          ></span>
+          <div>
+            <span
+              v-if="collapsed"
+              @click="collapsed = !collapsed"
+              class="pi pi-chevron-down"
+            ></span>
+            <span
+              v-else
+              @click="collapsed = !collapsed"
+              class="pi pi-chevron-up"
+            ></span>
+          </div>
         </template>
         <template #header>
-          <div>
-            <div
-              class="prose mb-1 flex h-full items-center p-2 dark:prose-invert"
-            >
-              <Avatar label="U" class="mr-2" shape="circle" size="small" />
-              <div class="flex flex-col">
-                <span
-                  class="prose prose-xl font-semibold leading-none dark:prose-invert"
-                  >@{{ post?.user.name ? post.user.name : "username" }}</span
-                >
-                <span class="prose prose-sm dark:prose-invert">{{
-                  post?.created_at
-                    ? dayjs(post.created_at).format("DD-MM-YYYY h:mm a")
-                    : "1 second ago"
-                }}</span>
-              </div>
+          <div
+            @click="headerClickFilter"
+            v-ripple
+            :id="`header-${post.id}`"
+            class="prose flex h-full w-full max-w-none items-center gap-2 p-2 dark:prose-invert"
+          >
+            <Avatar
+              :label="post.user.name.charAt(0)"
+              ass="mr-2"
+              shape="circle"
+              size="small"
+            />
+            <div class="flex flex-col">
+              <span
+                class="prose prose-xl font-semibold leading-none dark:prose-invert"
+                >@{{ post?.user.name ? post.user.name : "username" }}</span
+              >
+              <span class="prose prose-sm dark:prose-invert">{{
+                post?.created_at
+                  ? dayjs(post.created_at).format("DD-MM-YYYY h:mm a")
+                  : "1 second ago"
+              }}</span>
             </div>
           </div>
         </template>
 
         <template #footer>
-          <PostActions :post="post"></PostActions>
+          <PostActions :key="post.id" :post="post"></PostActions>
         </template>
 
         <div
-          class="mt- prose flex max-w-none flex-col gap-5 p-2 dark:prose-invert"
+          v-ripple
+          class="prose flex max-w-none flex-col gap-5 p-2 dark:prose-invert"
         >
           <span class="text-xl font-semibold leading-tight">
             {{ post?.title ? post?.title : "Title" }}
           </span>
 
           <div
-            class="place-items-center text-xl prose mt-2 grid grid-rows-[5fr,1fr] max-w-none dark:prose-invert gap-6 text-center leading-normal md:grid-rows-[7fr,1fr]"
+            @click="viewPost"
+            class="prose mt-2 grid max-w-none grid-rows-[5fr,1fr] place-items-center gap-6 text-center text-xl leading-normal dark:prose-invert md:grid-rows-[7fr,1fr]"
           >
             <span class="font-medium">
               "{{ post?.quote ? post.quote : "This is the quote" }}"
@@ -84,7 +110,7 @@ let collapsed = ref(false);
 <style>
 .p-panel-header {
   padding: 5px;
-  @apply border-b;
+  @apply border-none;
 }
 .p-panel-content {
   padding: 5px;
