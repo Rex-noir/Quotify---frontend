b6ef2c30782c69e7c6ae29b322957ed8ca3719a2
Lala <ace.rexnoir@gmail.com>
2024-07-12 22:05:51 +0530

    feat : methods fetchComments,loadComments

--- src/utils/post.utils.ts
+++ src/utils/post.utils.ts
@@ -1,13 +1,33 @@
 import api from "@/config/api.config";
-import type { Post } from "@/types/Post/post.types";
+import type { Post, PostComment } from "@/types/Post/post.types";
 import type { PaginatedResponse } from "@/types/Response/apiresponses.types";
 
 export default class PostUtils {
   static async fetchPosts(
     url?: string,
-  ): Promise<PaginatedResponse<Post[]> | Post> {
+  ): Promise<PaginatedResponse<Post> | Post> {
     try {
       const response = await api.get(url || "/posts");
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+  static async fetchComments(
+    id: number,
+    url?: string,
+  ): Promise<PaginatedResponse<PostComment>> {
+    try {
+      const response = await api.get(url || `/posts/${id}/comments`);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async loadComments(id: number): Promise<PostComment[]> {
+    try {
+      const response = await api.get(`/comments/replies/${id}`);
       return response.data;
     } catch (error) {
       throw error;
