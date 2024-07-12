43de075abdd60b2ae9035f14806cc318db8c2d13
Lala <ace.rexnoir@gmail.com>
2024-07-12 22:05:08 +0530

    feat : add variables and actions for current post

--- src/stores/posts.store.ts
+++ src/stores/posts.store.ts
@@ -1,15 +1,21 @@
-import { PostStyles } from "@/types/Post/post.types";
+import { PostStyles, type Post } from "@/types/Post/post.types";
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
 const usePostStore = defineStore("post", () => {
   const post_style = ref<PostStyles>(PostStyles.CARD);
 
+  const currentPost = ref<Post>();
+
   function setStyle(style: PostStyles) {
     post_style.value = style;
   }
 
-  return { post_style, setStyle };
+  function setCurrentPost(post: Post) {
+    currentPost.value = post;
+  }
+
+  return { post_style, setStyle, currentPost, setCurrentPost };
 });
 
 export default usePostStore;
