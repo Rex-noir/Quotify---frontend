1fe691ccc27556ccbbd31dc01f8a2549be9179b1
Lala <ace.rexnoir@gmail.com>
2024-07-13 22:39:58 +0530

    feat : scrollViewPosition, currentComment,  and methods of them

--- src/stores/posts.store.ts
+++ src/stores/posts.store.ts
@@ -1,11 +1,16 @@
-import { PostStyles, type Post } from "@/types/Post/post.types";
+import {
+  PostStyles,
+  type Post,
+  type PostComment,
+} from "@/types/Post/post.types";
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
 const usePostStore = defineStore("post", () => {
   const post_style = ref<PostStyles>(PostStyles.CARD);
-
+  const scrollPosition = ref<number | null>(null);
   const currentPost = ref<Post>();
+  const currentComment = ref<PostComment>();
 
   function setStyle(style: PostStyles) {
     post_style.value = style;
@@ -15,7 +20,24 @@ const usePostStore = defineStore("post", () => {
     currentPost.value = post;
   }
 
-  return { post_style, setStyle, currentPost, setCurrentPost };
+  function setCurrentComment(comment: PostComment) {
+    currentComment.value = comment;
+  }
+
+  function setScrollPosition(position: number) {
+    scrollPosition.value = position;
+  }
+
+  return {
+    post_style,
+    setStyle,
+    currentPost,
+    scrollPosition,
+    setScrollPosition,
+    setCurrentPost,
+    currentComment,
+    setCurrentComment,
+  };
 });
 
 export default usePostStore;
