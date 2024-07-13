e7e229f9be1a830d4e690aabf80c72c8f03e48b0
Lala <ace.rexnoir@gmail.com>
2024-07-13 22:40:50 +0530

    remove : NestedComments

--- src/types/Layouts/layouts.types.ts
+++ src/types/Layouts/layouts.types.ts
@@ -2,6 +2,7 @@ import HomeLayout from "@/Layouts/HomeLayout.vue";
 import AuthLayout from "@/Layouts/AuthLayout.vue";
 import SplashLayout from "@/Layouts/SplashLayout.vue";
 import PostLayout from "@/Layouts/PostLayout.vue";
+import NestedComments from "@/Layouts/NestedComments.vue";
 import type { DefineComponent } from "vue";
 
 export enum LayoutNames {
@@ -9,10 +10,12 @@ export enum LayoutNames {
   AUTH = "AUTH",
   SPLASH = "SPLASH",
   POST = "POST",
+  NESTED_COMMENTS = "NESTED_COMMENTS",
 }
 export const layouts: { [key in LayoutNames]: DefineComponent } = {
   [LayoutNames.HOME]: HomeLayout as DefineComponent,
   [LayoutNames.AUTH]: AuthLayout as DefineComponent,
   [LayoutNames.SPLASH]: SplashLayout as DefineComponent,
   [LayoutNames.POST]: PostLayout as DefineComponent,
+  [LayoutNames.NESTED_COMMENTS]: NestedComments as DefineComponent,
 };
