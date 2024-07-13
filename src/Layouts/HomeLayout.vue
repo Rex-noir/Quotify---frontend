39c17e04b8b63c39fd01c0cf4c2c24b7bc9e4ce1
Lala <ace.rexnoir@gmail.com>
2024-07-13 22:37:45 +0530

    refactor :  use router-view instead of HomView store keep-alive

--- src/Layouts/HomeLayout.vue
+++ src/Layouts/HomeLayout.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import HeaderSmall from "@/components/Headers/HeaderSmall.vue";
 import HeaderLarge from "@/components/Headers/HeaderLarge.vue";
-import useHomeViews from "@/stores/homeviews.store";
 import CommunitiesView from "@/views/Home/CommunitiesView.vue";
 import SideLoggedIn from "@/components/SideBar/SideLoggedIn.vue";
 import SideNotLoggedIn from "@/components/SideBar/SideNotLoggedIn.vue";
 import useUserStore from "@/stores/user.store";
+import PostListView from "@/views/Home/PostListView.vue";
 
 const userStore = useUserStore();
 </script>
 <template>
-  <div class="relative h-full">
+  <div class="h-ful relative">
     <HeaderSmall
       class="fixed z-50 w-full bg-surface-100 dark:bg-surface-0 lg:hidden"
     />
@@ -18,7 +18,7 @@ const userStore = useUserStore();
       class="fixed z-50 hidden w-full bg-surface-100 dark:bg-surface-0 lg:grid"
     />
     <div
-      class="relative top-[6rem] grid h-full md:top-[3.4rem] md:grid-cols-3 lg:top-11 lg:grid-cols-[250px,1fr,1fr,1fr]"
+      class="relative top-[7rem] grid h-full md:top-[3.6rem] md:grid-cols-3 lg:top-12 lg:grid-cols-[250px,1fr,1fr,1fr]"
     >
       <div
         class="hidden h-screen lg:fixed lg:col-start-1 lg:block lg:w-80 lg:max-w-[250px]"
@@ -26,10 +26,19 @@ const userStore = useUserStore();
         <component v-if="userStore.status" :is="SideLoggedIn" />
         <component v-else :is="SideNotLoggedIn" />
       </div>
-      <component
+      <!-- <component
         class="border-l border-r dark:border-surface-700 md:col-span-2 lg:col-span-2 lg:col-start-2"
         :is="useHomeViews().currentView"
-      />
+      /> -->
+      <div
+        class="border-l border-r dark:border-surface-700 md:col-span-2 lg:col-span-2 lg:col-start-2"
+      >
+        <router-view v-slot="{ Component }">
+          <keep-alive include="PostListView,QuoteView">
+            <component :is="Component" :key="$route.fullPath" />
+          </keep-alive>
+        </router-view>
+      </div>
       <component
         class="hidden bg-surface-100 md:fixed md:col-start-3 md:block lg:col-start-4"
         :is="CommunitiesView"
