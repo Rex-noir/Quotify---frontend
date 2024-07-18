13bb8f583ef5e72f817d3ef480f3e54228492d5c
Lala <ace.rexnoir@gmail.com>
2024-07-18 09:53:37 +0530

    feat : implement debounce fn

---
+++ src/utils/utils.ts
@@ -0,0 +1,15 @@
+export function debounce<T extends (...args: any[]) => void>(
+  func: T,
+  delay: number,
+): (...args: Parameters<T>) => void {
+  let timeoutId: NodeJS.Timeout;
+
+  return function (this: ThisParameterType<T>, ...args: Parameters<T>) {
+    const context = this;
+
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => {
+      func.apply(context, args);
+    }, delay);
+  };
+}
