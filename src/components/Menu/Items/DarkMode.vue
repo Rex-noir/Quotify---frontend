bc9e190a5a0d1b1f07e8fdd95c7586d7a85636b9
Lala <ace.rexnoir@gmail.com>
2024-07-22 14:59:43 +0530

    feat : item for menu

---
+++ src/components/Menu/Items/DarkMode.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import usePrefStore from "@/stores/preferences.store";
+import ToggleSwitch from "primevue/toggleswitch";
+import { computed, ref } from "vue";
+
+const prefStore = usePrefStore();
+const switchModel = computed(() => prefStore.isDark);
+</script>
+<template>
+  <div>
+    <div @click="prefStore.toggleDarkMode" class="flex w-full justify-between">
+      DarkMode
+      <ToggleSwitch v-model="switchModel" />
+    </div>
+  </div>
+</template>
