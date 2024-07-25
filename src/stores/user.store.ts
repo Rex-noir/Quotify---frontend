2d13825cfbf2e11b93002579ed545b26fe181b2b
Lala <ace.rexnoir@gmail.com>
2024-07-25 17:51:17 +0530

    feat : listen to user specific post updates channel

--- src/stores/user.store.ts
+++ src/stores/user.store.ts
@@ -1,11 +1,14 @@
+import type { Post } from "@/types/Post/post.types";
 import type { User } from "@/types/User/user.types";
 import { defineStore } from "pinia";
 import { ref } from "vue";
+import usePostStore from "./posts.store";
 
 const useUserStore = defineStore("user", () => {
   const status = ref<boolean>();
   const userInfo = ref<User | null>(null);
   const loading = ref<boolean>(true);
+  const postStore = usePostStore();
 
   function logOut() {
     status.value = false;
@@ -19,7 +22,28 @@ const useUserStore = defineStore("user", () => {
     loading.value = false;
   }
 
-  return { status, userInfo, logOut, logIn, loading };
+  function startListening() {
+    window.Echo.private(`App.Models.User.${userInfo.value?.id}`).listen(
+      "UserSpecificPostUpdates",
+      (e: { updates: Partial<Post> }) => {
+        postStore.updatePost(e.updates);
+      },
+    );
+  }
+
+  function stopListening() {
+    window.Echo.leave(`App.Models.User.${userInfo.value?.id}`);
+  }
+
+  return {
+    status,
+    userInfo,
+    logOut,
+    logIn,
+    loading,
+    startListening,
+    stopListening,
+  };
 });
 
 export default useUserStore;
