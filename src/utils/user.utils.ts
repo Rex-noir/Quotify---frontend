46af79ea3b660e40f22619bdd3196ea39fcde82f
Lala <ace.rexnoir@gmail.com>
2024-07-27 17:48:21 +0530

    feat : add searching users fn

--- src/utils/user.utils.ts
+++ src/utils/user.utils.ts
@@ -1,10 +1,28 @@
 import api from "@/config/api.config";
+import type { PaginatedResponse } from "@/types/Response/apiresponses.types";
 import type { User } from "@/types/User/user.types";
 
 export default class UserUtils {
   static async getUser(id: number): Promise<User> {
     try {
       const response = await api.get(`users/${id}`);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  static async searchUsers(
+    keyword: string,
+    limit?: number,
+  ): Promise<User[] | Promise<PaginatedResponse<User>>> {
+    try {
+      const response = await api.get("/users/search", {
+        params: {
+          keyword: keyword,
+          limit: limit,
+        },
+      });
       return response.data;
     } catch (error) {
       throw error;
