657a0f57809ae52eb38945f3fc7ecb4c4d5855d4
Lala <ace.rexnoir@gmail.com>
2024-07-27 17:46:42 +0530

    refactor : if the user props is present use that user's info

--- src/components/Menu/Items/Profile.vue
+++ src/components/Menu/Items/Profile.vue
@@ -1,20 +1,23 @@
 <script setup lang="ts">
 import useUserStore from "@/stores/user.store";
+import type { User } from "@/types/User/user.types";
 import Avatar from "primevue/avatar";
 const userStore = useUserStore();
+
+const props = defineProps<{
+  user?: User;
+}>();
+
+const userInfo = props.user ? props.user : userStore.userInfo;
 </script>
 <template>
   <div v-ripple>
     <div class="flex justify-center">
-      <Avatar
-        shape="circle"
-        :size="'small'"
-        :label="userStore.userInfo?.name[0]"
-      />
+      <Avatar shape="circle" :size="'small'" :label="userInfo?.name[0]" />
     </div>
     <div class="prose flex flex-col leading-[1.1] dark:prose-invert">
-      <span class="m-0 p-0 font-bold">{{ userStore.userInfo?.name }}</span>
-      <span class="m-0 p-0 text-sm">@{{ userStore.userInfo?.username }}</span>
+      <span class="m-0 p-0 font-bold">{{ userInfo?.name }}</span>
+      <span class="m-0 p-0 text-sm">@{{ userInfo?.username }}</span>
     </div>
   </div>
 </template>
